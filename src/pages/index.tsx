a8ae998af754f6d543475fdff10d08d9625f65de
Matt March <matthewmarch29@gmail.com>
2020-05-07 18:02:23 +0100

    Add a brief welcome to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,8 @@ import Layout from "../components/layout"
 
 export default () => (
 <Layout>
-  <h1>Welcome to my Homepage</h1>
-  Hello world!
+  <h1>Welcome, I'm Matt March, and welcome to my website</h1>
+  <p>I've just recreated my website using Gatsby and Typescript, and hosted for free with Netlify.</p>
+  <p>Previously I had a very empty blog using Jekyll on Github Pages, but this time will (maybe) be different.</p>
 </Layout>
 )
