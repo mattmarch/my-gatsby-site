0f82bf3382a454ca9c73850c45eb8fc9bfd87dd7
Matt March <matthewmarch29@gmail.com>
2020-05-15 14:23:08 +0100

    Change welcome message to not repeat welcome

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 
 const Index = () => (
   <Layout>
-    <h1>Welcome, I'm Matt March, and welcome to my website</h1>
+    <h1>Hi, I'm Matt March, and welcome to my website</h1>
     <p>
       I've just recreated my website using Gatsby and Typescript, check out my <Link to="/new-website">short write up of why and how here</Link>.
     </p>
