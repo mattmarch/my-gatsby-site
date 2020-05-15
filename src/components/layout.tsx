df2880deaa496d657d6129803858eec65a9a51bd
Matt March <matthewmarch29@gmail.com>
2020-05-15 17:06:39 +0100

    Add footer with copywrite

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -14,12 +14,18 @@ const Layout = ({ children }: Props) => (
     </Helmet>
     <Header />
     <ContentContainer>{children}</ContentContainer>
+    <Footer>
+        <hr/>
+        © 2020 Matt March
+    </Footer>
   </Page>
 )
 
 export default Layout
 
 const Page = styled.div`
+  display: flex;
+  flex-direction: column;
   height: 100%;
   margin-top: 0px;
 `
@@ -28,4 +34,11 @@ const ContentContainer = styled.div`
   margin: 3rem auto;
   max-width: 650px;
   padding: 0 1rem;
+  flex-grow: 1;
+`
+
+const Footer = styled.div`
+  font-size: 0.8rem;
+  padding: 1rem;
+  text-align: center;
 `
