9d7157af90ef36c36a16ce5f0b81086d78430931
Matt March <matthewmarch29@gmail.com>
2020-05-27 09:33:08 +0100

    Add contact links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,14 @@ const Index = () => (
   <Layout>
     <h1>Hi, I'm Matt March, and welcome to my website</h1>
     <p>
+      I'm a Bristol based software engineer currently working at <a href="https://www.ghyston.com/">Ghyston</a>. You can find me on:
+    </p>
+    <ul>
+      <li><a href="https://github.com/mattmarch">Github</a></li>
+      <li><a href="https://www.linkedin.com/in/matthew-march-a75b21121/">LinkedIn</a></li>
+      <li><a href="mailto:me@mattmarch.co.uk">Email</a></li>
+    </ul>
+    <p>
       I've just recreated my website using Gatsby and Typescript, check out my <Link to="/new-website">short write up of why and how here</Link>.
     </p>
     <p>
